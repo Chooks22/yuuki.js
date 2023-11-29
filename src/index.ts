49d315563825a33fd1ce1f0c1da7d32ff078aa05
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2023-11-29 15:20:36 +0800

    📦 NEW: expose types

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Intents, Permissions } from './dev-client.js'
+export type { IntentString, PermissionString, YuukiChatInputCommand, YuukiCommand, YuukiConfig, YuukiMessageCommand, YuukiUserCommand } from './dev-client.js'
